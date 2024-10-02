9f568dc1cc8ccdd533181b0ba60dfe370583937e
Raihanhkma <raihananwar205@gmail.com>
2024-10-02 22:35:42 +0700

    Add files via upload

---
+++ LAB QUIZ raihan.py
@@ -0,0 +1,20 @@
+class binusmaya:
+    def __init__(self,lecturers,classes,schedule):
+        self.lecturers=lecturers
+        self.classes=classes
+        self.schedule=schedule
+
+data=binusmaya ([{'name':"lina", 'ID':'222333','name':'lisa', 'ID':'666666','name':'jane','ID':'999999'}]
+['L1AC','L1AB','Lacv1'],
+(''))
+
+def asslect():
+    new_lect={'name':'jane','ID':'8888','subject':'religion'}
+    for value in data.lecturers:
+        if value == new_lect
+    data.lecturers.
+
+    return data.lecturers
+
+print(addlect())
+
